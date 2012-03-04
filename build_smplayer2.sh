cdf66d88555dc8e961b29709ec2b6259b31f9331
Paul Dersey <pdersey@gmail.com>
2012-03-03 20:26:45 -0500

    update SMPlayer2 build to link with quazip for subtitle downloading

--- build_smplayer2.sh
+++ build_smplayer2.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
 export PATH=$PNDSDK/usr/bin:$PATH 
-export CFLAGS="-DPANDORA -O2 -pipe -march=armv7-a -mcpu=cortex-a8 -mtune=cortex-a8 -mfpu=neon -ftree-vectorize -mfloat-abi=soft -ffast-math -fsingle-precision-constant -fno-inline-functions" 
-export CXXFLAGS="-DPANDORA -O2 -pipe -march=armv7-a -mcpu=cortex-a8 -mtune=cortex-a8 -mfpu=neon -ftree-vectorize -mfloat-abi=soft -ffast-math -fsingle-precision-constant -fno-inline-functions" 
+export CFLAGS="-DPANDORA -O2 -pipe -march=armv7-a -mcpu=cortex-a8 -mtune=cortex-a8 -mfpu=neon -ftree-vectorize -mfloat-abi=softfp -ffast-math -fsingle-precision-constant -fno-inline-functions" 
+export CXXFLAGS="-DPANDORA -O2 -pipe -march=armv7-a -mcpu=cortex-a8 -mtune=cortex-a8 -mfpu=neon -ftree-vectorize -mfloat-abi=softfp -ffast-math -fsingle-precision-constant -fno-inline-functions" 
+
+cd quazip-0.4.4
+qmake -spec $PNDSDK/../sdk_utils/qmake_linux-pandora-g++ 
+make sub-quazip
+make -C quazip staticlib
+cd ..
+
+# Apply patch to SMPlayer2 CMakefiles.txt to use local quazip build
+git apply --directory=SMPlayer2 patches/smplayer2-quazip-build.patch
+
+# copy zlib headers into SMPlayer2/src directory to work around build problems caused by adding $PNDSDK/usr/include to include path
+cp $PNDSDK/usr/include/zlib.h SMPlayer2/src
+cp $PNDSDK/usr/include/zconf.h SMPlayer2/src
 
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=release -DENABLE_STRICT_COMPILATION=off -DCMAKE_TOOLCHAIN_FILE=$PNDSDK/../sdk_utils/PandoraToolchain.cmake -DPKG_CONFIG_EXECUTABLE=$PNDSDK/arm-none-linux-gnueabi-pkg-config -DSDL_CONFIG=$PNDSDK/usr/bin/sdl-config -DLUA_MATH_LIBRARY=$PNDSDK/arm-none-linux-gnueabi/libc/usr/lib/libm.so ../SMPlayer2
+cmake -DCMAKE_BUILD_TYPE=release \
+      -DCMAKE_TOOLCHAIN_FILE=$PNDSDK/../sdk_utils/PandoraToolchain.cmake \
+      -DQUAZIP_INCLUDE_DIR=../quazip-0.4.4 \
+      -DQUAZIP_LIBRARY=../quazip-0.4.4/quazip/libquazip.a \
+      ../SMPlayer2
 make
